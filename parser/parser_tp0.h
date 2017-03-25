20031ee938133e26e8373a1467ebef2a033114d8
Martin Errazquin <martin.errazquin@hotmail.com>
2017-03-25 18:08:52 -0300

    agregado parser

---
+++ parser/parser_tp0.h
@@ -0,0 +1,26 @@
+struct run_data {
+	char is_decode;
+	char print_help;
+	char print_version;
+	char error_flag;	
+	FILE* input;
+	FILE* output;
+};
+
+typedef struct run_data run_data_t;
+
+//Set default values on given run_data_t ref
+void set_default(run_data_t* rd);
+
+//Try to set action according to arg, if succesful returns 0.
+char set_action(run_data_t* rd, char* arg);
+
+//Try to set input according to arg, if succesful returns 0.
+char set_input(run_data_t* rd, char* arg);
+
+//Try to set output according to arg, if succesful returns 0.
+char set_output(run_data_t* rd, char* arg);
+
+//Parse options given from cmd line
+//Should an error occur, prints it to stderr and sets print_help with stdout as output
+void parse_cmd(run_data_t* rd, int argc, char** argv);
