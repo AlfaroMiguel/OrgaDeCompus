7ae7762ee5b1a76c0c5d3d8f8a585e8205a456d6
Agustina <abmendez25@hotmail.com>
2017-04-08 19:04:46 -0300

    I/O check

--- tp0.c
+++ tp0.c
@@ -14,6 +14,16 @@
 #define SOURCE_CODE_SIZE_DECODE 4
 #define RESULT_SIZE_DECODE 3
 
+#define VERSION_MSG "Version %s\n"
+#define ARGUMENT_ERROR_MSG "ARGUMENT ERROR\n"
+#define DECODING_ERROR_MSG "DECODING ERROR\n"
+#define INPUT_OPENING_ERROR_MSG "ERROR OPENING INPUT FILE \n"
+#define OUTPUT_OPENING_ERROR_MSG "ERROR OPENING OUTPUT FILE \n"
+#define FILE_WRITING_ERROR_MSG "ERROR WRITING IN FILE\n"
+#define FILE_READING_ERROR_MSG "ERROR READING FROM FILE\n"
+
+#define STD_FILE_SYMBOL "-"
+
 void print_help(){
 	FILE* help_file = fopen(HELP_FILE, "r");
 	char c = fgetc(help_file);
@@ -26,17 +36,17 @@ void print_help(){
 }
 
 void print_version(){
-	printf("Version %s\n", VERSION);
+	printf(VERSION_MSG, VERSION);
 }
 
 FILE* get_input_file(char* input){
-	if (!input || !strcmp(input, "-"))
+	if (!input || !strcmp(input, STD_FILE_SYMBOL))
 		return stdin;
 	return fopen(input, "rb");
 }
 
 FILE* get_output_file(char* output){
-	if (!output || !strcmp(output, "-"))
+	if (!output || !strcmp(output, STD_FILE_SYMBOL))
 		return stdout;
 	return fopen(output, "wb");
 }
@@ -50,10 +60,19 @@ void close_files(FILE* input, FILE* output){
 
 void encode(char* input, char* output){
 	FILE* input_file = get_input_file(input);
+	if (! input_file) {
+		fprintf(stderr, INPUT_OPENING_ERROR_MSG);
+		return;
+	}
 	FILE* output_file = get_output_file(output);
+	if (! output_file) {
+		fprintf(stderr, OUTPUT_OPENING_ERROR_MSG);
+		close_files(input_file, output_file);
+		return;
+	}
 	unsigned char source_code[SOURCE_CODE_SIZE_ENCODE];
 	unsigned char result[RESULT_SIZE_ENCODE];
-	int i, char_to_encode;
+	int i, char_to_encode, bytes_written;
 	int c = fgetc(input_file);
 	while (c != EOF){
 		i = 0;
@@ -70,22 +89,33 @@ void encode(char* input, char* output){
 			i++;
 		}
 		base64_encode(source_code, result, char_to_encode);
-		fwrite(result, 1, 4, output_file);
+		bytes_written = fwrite(result, 1, 4, output_file);
+		if (bytes_written == 0){
+			fprintf(stderr, FILE_WRITING_ERROR_MSG);
+			break;
+		}
 	}
 
 	if(ferror(input_file))
-		fprintf(stderr, "ERROR READING FILE %s", input);
-
-
+		fprintf(stderr, FILE_READING_ERROR_MSG);
 	close_files(input_file, output_file);
 }
 
 void decode(char* input, char* output){
 	FILE* input_file = get_input_file(input);
+	if (! input_file) {
+		fprintf(stderr, INPUT_OPENING_ERROR_MSG);
+		return;
+	}
 	FILE* output_file = get_output_file(output);
+	if (! output_file) {
+		fprintf(stderr, OUTPUT_OPENING_ERROR_MSG);
+		close_files(input_file, output_file);
+		return;
+	}
 	unsigned char source_code[SOURCE_CODE_SIZE_DECODE];
 	unsigned char result[RESULT_SIZE_DECODE];
-	int i, write;
+	int i, write, bytes_written;
 	int c = fgetc(input_file);
 	while (c != EOF){
 		i = 0;
@@ -101,16 +131,18 @@ void decode(char* input, char* output){
 		}
 		bool success = base64_decode(source_code, result, &write);
 		if (!success){
-			fprintf(stderr, "Decoding Error\n");
-			return;
+			fprintf(stderr, DECODING_ERROR_MSG);
+			break;
 		}
-        fwrite(result, 1, write, output_file);
+        bytes_written = fwrite(result, 1, write, output_file);
+        if (bytes_written == 0){
+			fprintf(stderr, FILE_WRITING_ERROR_MSG);
+			break;
+		}
 	}
 
 	if(ferror(input_file))
-		fprintf(stderr, "ERROR READING FILE %s", input);
-
-
+		fprintf(stderr, FILE_READING_ERROR_MSG);
 	close_files(input_file, output_file);
 }
 
@@ -129,7 +161,7 @@ int main(int argc, char* argv[]){
 	else if(!rd->error_flag){
 		encode(rd->input, rd->output);
 	}
-	else fprintf(stderr, "Argument Error\n");
+	else fprintf(stderr, ARGUMENT_ERROR_MSG);
 	free(rd);
 	return 0;
 }
