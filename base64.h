204f67782d6e46a840479ed74366d7ed7c068903
Agustina <abmendez25@hotmail.com>
2017-05-01 15:53:45 -0300

    main y base64.h del tp1

---
+++ base64.h
@@ -0,0 +1,57 @@
+#ifndef _BASE64_H
+#define _BASE64_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+/*DEFINICION CONSTANTES*/
+#define EMPTYBASE256 '\0'
+#define EMPTYBASE64 '='
+#define PLUS '+'
+#define SLASH '/'
+
+#define A_ASCII 65
+#define Z_ASCII 90
+#define A_MIN_ASCII 97
+#define Z_MIN_ASCII 122
+#define ZERO_ASCII 48
+#define NINE_ASCII 57
+
+#define OFFSET1 65
+#define OFFSET2 71
+#define OFFSET3 4
+#define PLUSB64 62
+#define SLASHB64 63
+
+static const char base64_table[] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+const char* errmsg[];
+/*Recibe:
+ *       Buffer con 3 caracteres en Base256
+ *       Buffer para guardar el resultado de la codificacion
+ *       Cantidad de caracteres a encodear (que no son padding)
+ *
+ *Codifica el contenido en Base256 a Base64 y lo guarda en el buffer resultado*/
+bool base64_encode(const unsigned char* source_code, unsigned char* result, int char_to_encode);
+
+/*Recibe:
+ *       Buffer conteniendo 4 bytes en Base64
+ *       Buffer de 3 bytes en Base256 para guardar el resultado
+ *       Puntero a int que apunta a un entero que guarda la cantidad de caracteres a escribir (que no son padding)
+ *
+ * Decodifica el contenido en Base64 y de ser una decodificacion existosa lo guarda
+ * en el buffer result y devuelve True. Sino, devuelve False*/
+bool base64_decode(const unsigned char* source_code, unsigned char* result, int* write);
+
+#endif
---
+++ tp1.c
@@ -0,0 +1,159 @@
+#include "parser/parser_tp0.h"
+#include "base64/base64.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define HELP_FILE "help.txt"
+#define VERSION "2.1.3"
+#define STDIN_FD 0
+#define STDOUT_FD 1
+
+#define SOURCE_CODE_SIZE_ENCODE 3
+#define RESULT_SIZE_ENCODE 4
+
+#define SOURCE_CODE_SIZE_DECODE 4
+#define RESULT_SIZE_DECODE 3
+
+#define VERSION_MSG "Version %s\n"
+#define ARGUMENT_ERROR_MSG "ARGUMENT ERROR\n"
+#define DECODING_ERROR_MSG "DECODING ERROR\n"
+#define INPUT_OPENING_ERROR_MSG "ERROR OPENING INPUT FILE \n"
+#define OUTPUT_OPENING_ERROR_MSG "ERROR OPENING OUTPUT FILE \n"
+#define FILE_WRITING_ERROR_MSG "ERROR WRITING IN FILE\n"
+#define FILE_READING_ERROR_MSG "ERROR READING FROM FILE\n"
+
+#define STD_FILE_SYMBOL "-"
+
+#define NO_ERROR_CODE 0
+#define DECODING_ERROR_CODE 1
+#define FILE_WRITING_ERROR_CODE 2
+#define FILE_READING_ERROR_CODE 3
+
+//#define MAX_ERROR_LEN 30
+
+// static const char* error_msg[3] = {DECODING_ERROR_MSG, FILE_WRITING_ERROR_MSG, FILE_READING_ERROR_MSG};
+// static char error_buf[MAX_ERROR_LEN];
+
+extern const char* errmsg[];
+
+void print_help(){
+	FILE* help_file = fopen(HELP_FILE, "r");
+	char c = fgetc(help_file);
+	while (c != EOF){
+		printf("%c", c);
+		c = fgetc(help_file);
+	}
+	printf("\n");
+	fclose(help_file);
+}
+
+void print_version(){
+	printf(VERSION_MSG, VERSION);
+}
+
+FILE* get_input_file(char* input){
+	if (!input || !strcmp(input, STD_FILE_SYMBOL))
+		return stdin;
+	return fopen(input, "rb");
+}
+
+FILE* get_output_file(char* output){
+	if (!output || !strcmp(output, STD_FILE_SYMBOL))
+		return stdout;
+	return fopen(output, "wb");
+}
+
+void close_files(FILE* input, FILE* output){
+	if (input != stdin)
+		fclose(input);
+	if (output != stdout)
+		fclose(output);
+}
+
+int encode(int input, int output){
+	unsigned char source_code[SOURCE_CODE_SIZE_ENCODE];
+	unsigned char result[RESULT_SIZE_ENCODE];
+	int i, char_to_encode, bytes_written;
+	int c = fgetc(input_file);
+	while (c != EOF){
+		i = 0;
+        char_to_encode = 0;
+		while (i < 3){
+			source_code[i] = c;
+			i++;
+            char_to_encode++;
+			c = fgetc(input_file);
+            if(c == EOF) break;
+		}
+		while (i < 3){
+			source_code[i] = '\0';
+			i++;
+		}
+		base64_encode(source_code, result, char_to_encode);
+		bytes_written = fwrite(result, 1, 4, output_file);
+		if (bytes_written == 0) return FILE_WRITING_ERROR_CODE;
+	}
+
+	if(ferror(input_file)) return FILE_READING_ERROR_CODE; 
+	return NO_ERROR_CODE;
+}
+
+int decode(int input, int output){
+	unsigned char source_code[SOURCE_CODE_SIZE_DECODE];
+	unsigned char result[RESULT_SIZE_DECODE];
+	int i, write, bytes_written;
+	int c = fgetc(input_file);
+	while (c != EOF){
+		i = 0;
+		while (i < 4){
+			source_code[i] = c;
+			i++;
+			c = fgetc(input_file);
+            if(c == EOF) break;
+		}
+		while (i < 4){
+			source_code[i] = '=';
+			i++;
+		}
+		bool success = base64_decode(source_code, result, &write);
+		if (!success) return DECODING_ERROR_CODE;
+        bytes_written = fwrite(result, 1, write, output_file);
+        if (bytes_written == 0) return FILE_WRITING_ERROR_CODE;
+	}
+
+	if(ferror(input_file)) return FILE_READING_ERROR_CODE;
+	return NO_ERROR_CODE;
+}
+
+char* get_error_msg(int error_code){
+	if (error_code > 3 || error_code < 1) snprintf()
+}
+int main(int argc, char* argv[]){
+	run_data_t* rd = malloc(sizeof(run_data_t));
+	parse_cmd(rd, argc, argv);
+	if (rd->print_help)
+		print_help();
+	if (rd->print_version){
+		print_version();
+		return 0;
+		}
+	int input_file = fileno(get_input_file(rd->input));
+	if (! input_file) {
+		fprintf(stderr, INPUT_OPENING_ERROR_MSG);
+		return 0;
+	}
+	int output_file = fileno(get_output_file(rd->output));
+	if (! output_file) {
+		fprintf(stderr, OUTPUT_OPENING_ERROR_MSG);
+		close_files(input_file, output_file);
+		return 0;
+	}
+	int res;
+	if (rd->is_decode) res = decode(input_file, output_file);
+	else if(!rd->error_flag) res = encode(input_file, output_file);
+	else fprintf(stderr, ARGUMENT_ERROR_MSG);
+	if (res != 0) fprintf(stderr, get_error_msg(res));
+	close_files(input_file, output_file);
+	free(rd);
+	return 0;
+}
